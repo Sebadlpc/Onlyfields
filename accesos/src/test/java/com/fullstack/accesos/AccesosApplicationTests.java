ba38ac40456a7e9e21c00785595ffcc734b054bc
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-23 17:09:40 -0400

    Primer commit

---
+++ accesos/src/test/java/com/fullstack/accesos/AccesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.accesos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ suscripciones/src/main/java/com/fullstack/suscripciones/SuscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.suscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuscripcionesApplication.class, args);
+	}
+
+}
