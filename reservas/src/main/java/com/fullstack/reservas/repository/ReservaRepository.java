48655ddf9d3ffd1ceecae9aa850c1b37d48060cb
Gabriel Ignacio Zurita Zijl <dolapezurita@gmail.com>
2026-04-28 19:32:32 -0400

    Agregar módulo de reservas con controller, dto, exceptions, model y repository

---
+++ reservas/src/main/java/com/fullstack/reservas/repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.fullstack.reservas.repository;
+
+import com.fullstack.reservas.model.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+}
