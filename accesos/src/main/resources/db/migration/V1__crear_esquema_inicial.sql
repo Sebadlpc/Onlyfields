09b52ac6e3241228281263f6d43e1a7987aec96d
Sebastian <sebadlpc@gmail.com>
2026-05-06 14:37:05 -0400

    Arreglo

--- accesos/src/main/resources/db/migration/V1__crear_esquema_inicial.sql
+++ accesos/src/main/resources/db/migration/V1__crear_esquema_inicial.sql
@@ -1,4 +1,3 @@
--- 1. Tabla de Tokens QR
 CREATE TABLE qr_tokens (
                            id NUMBER(19,0) GENERATED AS IDENTITY,
                            cliente_id NUMBER(19,0) NOT NULL,
@@ -9,7 +8,7 @@ CREATE TABLE qr_tokens (
                            CONSTRAINT uk_qr_tokens_token UNIQUE (token)
 );
 
--- 2. Tabla de Registro de Accesos
+
 CREATE TABLE registro_acceso (
                                  id NUMBER(19,0) GENERATED AS IDENTITY,
                                  cliente_id NUMBER(19,0) NOT NULL,
@@ -18,7 +17,7 @@ CREATE TABLE registro_acceso (
                                  fecha_hora TIMESTAMP NOT NULL,
                                  motivo_rechazo VARCHAR2(255),
                                  CONSTRAINT pk_registro_acceso PRIMARY KEY (id),
-    -- Validación de Enums reales según código Java
+   
                                  CONSTRAINT chk_tipo_acceso CHECK (tipo IN ('ENTRADA', 'SALIDA')),
                                  CONSTRAINT chk_resultado_acceso CHECK (resultado IN ('PERMITIDO', 'DENEGADO'))
 );
