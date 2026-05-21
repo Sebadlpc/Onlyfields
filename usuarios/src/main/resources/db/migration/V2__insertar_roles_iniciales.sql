10aa1f1b4dfc27908f427ef5fb473f94cccadc4e
SerjioKLO <sergi.sepulvedas@duocuc.cl>
2026-05-20 20:40:54 -0400

    Finalmente

--- usuarios/src/main/resources/db/migration/V2__insertar_roles_iniciales.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO rol (nombre, descripcion) VALUES ('ADMIN', 'Administrador del sistema');
-INSERT INTO rol (nombre, descripcion) VALUES ('CREADOR', 'Creador de contenido');
-INSERT INTO rol (nombre, descripcion) VALUES ('CLIENTE', 'Usuario consumidor');
