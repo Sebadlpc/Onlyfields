234e2bed8b3f1f83b6347b0a001b05cab26dd47f
Gabriel Ignacio Zurita Zijl <dolapezurita@gmail.com>
2026-06-21 20:47:56 -0400

    act

--- reservas/src/main/java/com/fullstack/reservas/dto/ReservaRequestDTO.java
+++ reservas/src/main/java/com/fullstack/reservas/dto/ReservaRequestDTO.java
@@ -1,5 +1,6 @@
 package com.fullstack.reservas.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -22,9 +23,11 @@ public class ReservaRequestDTO {
 
     @NotNull
     @Future
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") // Indica cómo leer el JSON
     private LocalDateTime fechaInicio;
 
     @NotNull
     @Future
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") // Indica cómo leer el JSON
     private LocalDateTime fechaFin;
 }
